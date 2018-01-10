298cf819c7a20d8a1cffd449b789d517f34598e3
briicoronel25 <31936829+briicoronel25@users.noreply.github.com>
2018-01-10 13:27:59 -0500

    Update photography.php
    adding gallery images and lightbox

--- PortfolioBriggite/photography.php
+++ PortfolioBriggite/photography.php
@@ -1,39 +1,89 @@
+
 <?php include ('views/header.php');?>
+
 <!-- !PAGE CONTENT! -->
 <div class="w3-main w3-content w3-padding" style="max-width:1200px;margin-top:100px">
 
+  <!-- First Photo Grid-->
+  <div class="w3-row-padding w3-padding-16 w3-center" id="food">
 
-<div class="gallery">
-  <a target="_blank" href="
+    <div class="w3-quarter">
+      <a href="#img1">
+        <img class="square" src="images/photography/carla-small.png" alt="Carla1" style="width:100%">
+      </a>
+      <a href="#_" class="lightbox" id="img1">
+        <img src="images/photography/carla.JPG">
+      </a>
+    </div>
+
+    <div class="w3-quarter">
+      <a href="#img2">
+        <img class="square" src="images/photography/carla2-small.png" alt="Carla2" style="width:100%">
+      </a>
+      <a href="#_" class="lightbox" id="img2">
+        <img src="images/photography/carla2.JPG">
+      </a>
+    </div>
+
+    <div class="w3-quarter">
+      <a href="#img3">
+        <img class="square" src="images/photography/carla3-small.png" alt="Carla2" style="width:100%">
+      </a>
+      <a href="#_" class="lightbox" id="img3">
+        <img src="images/photography/carla3.JPG">
+      </a>
+    </div>
+
+    <div class="w3-quarter">
+      <a href="#img4">
+        <img class="square" src="images/photography/carla7-small.png" alt="Carla2" style="width:100%">
+      </a>
+      <a href="#_" class="lightbox" id="img4">
+        <img src="images/photography/carla7.JPG">
+      </a>
+    </div>
+  </div>
 
 
-  building5">
-    <img src="building5" alt="place1" width="300" height="200">
-  </a>
-  <div class="desc">Add a description of the image here</div>
-</div>
+  <!-- second Photo Grid-->
+  <div class="w3-row-padding w3-padding-16 w3-center">
 
-<div class="gallery">
-  <a target="_blank" href="forest.jpg">
-    <img src="forest.jpg" alt="Forest" width="300" height="200">
-  </a>
-  <div class="desc">Add a description of the image here</div>
-</div>
+    <div class="w3-quarter">
+      <a href="#img9">
+        <img class="square" src="images/photography/place-small.png" alt="Carla2" style="width:100%">
+      </a>
+      <a href="#_" class="lightbox" id="img9">
+        <img src="images/photography/place.JPG">
+      </a>
+    </div>
 
-<div class="gallery">
-  <a target="_blank" href="lights.jpg">
-    <img src="lights.jpg" alt="Northern Lights" width="300" height="200">
-  </a>
-  <div class="desc">Add a description of the image here</div>
-</div>
+    <div class="w3-quarter">
+      <a href="#img10">
+        <img class="square" src="images/photography/building5-small.png" alt="Carla2" style="width:100%">
+      </a>
+      <a href="#_" class="lightbox" id="img10">
+        <img src="images/photography/building5.JPG">
+      </a>
+    </div>
 
-<div class="gallery">
-  <a target="_blank" href="mountains.jpg">
-    <img src="mountains.jpg" alt="Mountains" width="300" height="200">
-  </a>
-  <div class="desc">Add a description of the image here</div>
-</div>
+    <div class="w3-quarter">
+      <a href="#img11">
+        <img class="square" src="images/photography/autumn2-small.png" alt="Carla2" style="width:100%">
+      </a>
+      <a href="#_" class="lightbox" id="img11">
+        <img src="images/photography/autumn2.JPG">
+      </a>
+    </div>
 
+    <div class="w3-quarter">
+      <a href="#img12">
+        <img class="square" src="images/photography/building-small.png" alt="Carla2" style="width:100%">
+      </a>
+      <a href="#_" class="lightbox" id="img12">
+        <img src="images/photography/building.JPG">
+      </a>
+    </div>
+  </div>
 
 <!-- Footer -->
 <?php include ('views/footer.php');?>
